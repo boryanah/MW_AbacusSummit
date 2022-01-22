b10abeab2dc7e00ca09b0e6e544b1a7ab438c0bf
Boryana <boryana.hadzhiyska@cfa.harvard.edu>
2022-01-22 09:47:01 -0800

    somewhat cleaner code with a couple of todo's; version I run for many simulations

--- build_tree.py
+++ build_tree.py
@@ -104,7 +104,7 @@ ind_stop = np.argmin(np.abs(zs_mt - z_stop))
 inds_fn_this = np.arange(n_superslabs, dtype=int)
 inds_fn_prev = np.arange(n_superslabs, dtype=int)
 
-# cosmological parameters  # TODO: automatize (only issue is sigma8); could alternatively take the age from the header files instead of colossus, but error should be small
+# cosmological parameters  # TODO: automatize (only issue is sigma8) or take age from header files instead of colossus
 omega_b = 0.02237
 omega_cdm = 0.1200
 omega_ncdm = 0.00064420
@@ -127,52 +127,42 @@ def func(z, target):
     return cc_cosm.lookbackTime(z) - target
 
 # Constraint 1: mass today TODO: would need to be corrected for evolution between z = 0.1 and z = 0
-mass_fin_lb = 0.9e12*h # Msun/h # og 
+mass_fin_lb = 0.9e12*h # Msun/h
 mass_fin_hb = 1.2e12*h # Msun/h
-#mass_fin_lb = 0.8e12*h # Msun/h # TESTING
 print("final mass logM", np.log10(mass_fin_lb), np.log10(mass_fin_hb))
 
 # Constraint 2: mergers with > 1.e11 systems
 # First merger LMC-like
 age_m1_lb = 1 # Gyr
-age_m1_hb = 3 # Gyr # og
-#age_m1_hb = 4 # Gyr # TESTING
+age_m1_hb = 3 # Gyr
 z_m1_lb = fsolve(func, args=(age_m1_lb), x0=0.)
 z_m1_hb = fsolve(func, args=(age_m1_hb), x0=0.)
 print("redshift range merger 1:", z_m1_lb, z_m1_hb)
 
 # Second merger Sagittarius-like
-age_m2_lb = 5 # Gyr # og
-#age_m2_lb = 4 # Gyr # TESTING
-age_m2_hb = 7 # Gyr # og
-#age_m2_hb = 8 # Gyr # TESTING
+age_m2_lb = 5 # Gyr
+age_m2_hb = 7 # Gyr
 z_m2_lb = fsolve(func, args=(age_m2_lb), x0=0.)
 z_m2_hb = fsolve(func, args=(age_m2_hb), x0=0.)
 print("redshift range merger 2:", z_m2_lb, z_m2_hb)
 
 # Third merger Gaia-Sausage-Enceladus-like
-age_m3_lb = 9 # Gyr # og
-#age_m3_lb = 8 # Gyr # TESTING
-age_m3_hb = 11 # Gyr # og
-#age_m3_hb = 10 # Gyr # TESTING
+age_m3_lb = 9 # Gyr
+age_m3_hb = 11 # Gyr
 z_m3_lb = fsolve(func, args=(age_m3_lb), x0=0.)
 z_m3_hb = fsolve(func, args=(age_m3_hb), x0=0.)
 print("redshift range merger 3:", z_m3_lb, z_m3_hb)
 
 # masses of all mergers
-mass_ms_lb = 0.8e11*h # Msun/h # og
-#mass_ms_lb = 1.e11*h # Msun/h # TESTING
-mass_ms_hb = 2.0e11*h # Msun/h # og
-#mass_ms_hb = 4.0e11*h # Msun/h # TESTING
+mass_ms_lb = 0.8e11*h # Msun/h
+mass_ms_hb = 2.0e11*h # Msun/h
 
 # Constraint 3: presence of Andromeda, single halo within 1 Mpc
-mass_fut_lb = 0.9e12*h # Msun/h # og
-#mass_fut_lb = 0.8e12*h # Msun/h # TESTING 
+mass_fut_lb = 0.9e12*h # Msun/h
 mass_fut_hb = 4.0e12*h # Msun/h
-dist_fut_lb = 0.55*h # Mpc/h # TESTING 0.6 og
-dist_fut_hb = 1.05*h # Mpc/h # TESTING 1.0 og
+dist_fut_lb = 0.6*h # Mpc/h
+dist_fut_hb = 1.0*h # Mpc/h
 dist_cl_hb = 3.0*h # Mpc/h
-#dist_cl_lb = 0.01*h # Mpc/h
 assert (mass_fut_hb >= mass_fin_hb) & (mass_fut_lb <= mass_fin_lb), "Need to adjust by hand"
 
 # radial velocity of andromeda
@@ -187,25 +177,17 @@ dist_fut_hb -= dist_rad # Mpc/h
 zs_m1 = (z_m1_lb < zs_mt) & (z_m1_hb >= zs_mt)
 zs_m2 = (z_m2_lb < zs_mt) & (z_m2_hb >= zs_mt)
 zs_m3 = (z_m3_lb < zs_mt) & (z_m3_hb >= zs_mt)
-# TESTING only comment out if using all redshifts rather than actual
+
+# should always add one below but not one above because merger occurs between this and prev, and we study progs at this (distinat prev)
 if np.argmax(zs_m1) != 0:
     zs_m1[np.argmax(zs_m1)-1] = True
 if np.argmax(zs_m2) != 0:
     zs_m2[np.argmax(zs_m2)-1] = True
 if np.argmax(zs_m3) != 0:
     zs_m3[np.argmax(zs_m3)-1] = True
-
 print("redshifts of interest for m1", zs_mt[zs_m1])
 print("redshifts of interest for m2", zs_mt[zs_m2])
 print("redshifts of interest for m3", zs_mt[zs_m3])
-
-# thoughts on redshift ranges:
-# the above redshift ranges refer to the upper and lower bounds when the mergers could have occurred
-# otoh, the abacus epochs are limited and somewhat coarse. For a given z_this where we have a halo
-# comprised of two smaller halos (1 = 2 + 3), and at z_prev we have 2 and 3 separate, that indicates
-# that the merger has occurred between z_this and z_prev; hence we always want to take the (no, ceil and)
-# floor of abacus redshifts to make sure we are thorough (this is not what I'm currently doing, but
-# also I worry about being too lenient)
 
 # fields to extract from the merger trees
 fields_mt_this = ['MainProgenitor', 'Progenitors', 'NumProgenitors']
@@ -239,17 +221,14 @@ info_this = Merger_this['MainProgenitor'] > 0
 # healthy halos (not potential splits)
 notsplit_this = Merger_this['IsPotentialSplit'] == 0
 
-# select Milky Way sized halos (by total mass)
-#mass_selection = (Merger_this['HaloMass'] > mass_fin_lb) & (Merger_this['HaloMass'] <= mass_fin_hb)
-
-# tuks load the cleaned halos and select halo masses that way (everything else can be the same -- doesn't need to have merger info and notsplit but I am pretty sure this is guaranteed
-print("loading")
+# load the cleaned halos and select halo masses
 sim_dir = f"/global/project/projectdirs/desi/cosmosim/Abacus/{sim_name:s}/halos/z{z_this:.3f}/halo_info/"
 cat = CompaSOHaloCatalog(sim_dir, load_subsamples=False, fields = ['N'])
 part_mass = cat.header['ParticleMassHMsun']
 mass_halo = cat.halos['N'].astype(np.float64) * part_mass
 mass_selection = (mass_halo > mass_fin_lb) & (mass_halo <= mass_fin_hb)
-print("loaded")
+del cat; gc.collect()
+print("loaded compaso catalog")
 
 # select MW-sized halos with a single Andromeda-like halo in the vicinity
 initial_selection = mass_selection & info_this & notsplit_this
@@ -258,8 +237,9 @@ index = Merger_this['HaloIndex'][initial_selection]
 position = Merger_this['Position'][initial_selection]+Lbox/2.
 N_mw = np.sum(initial_selection)
 index_all = np.arange(len(mass_selection), dtype=int)
-np.save(f"data/inds_mw_{sim_name:s}.npy", index_all[initial_selection]); quit()
-print("number of Milky Way sized halos with merger info", np.sum(initial_selection)) # teeny fraction don't have it
+
+np.save(f"data/inds_mw_{sim_name:s}.npy", index_all[initial_selection]); del index_all; gc.collect()
+print("number of Milky Way sized halos with merger info", np.sum(initial_selection))
 
 # halos to build main progenitor
 main_progs = correct_inds(main_progs, N_halos_slabs_prev, slabs_prev, inds_fn_prev)
@@ -274,28 +254,26 @@ n_merger_above_m3 = np.zeros(N_mw, dtype=int)
 missing = np.ones(N_mw, dtype=bool)
 andromeda = np.zeros(N_mw, dtype=bool)
 
-# start TESTING just to make it faster
-# would select Andromeda-like galaxies and find their positions and then compute the distance for each milky way one maybe by building a tree  and finding how many are within spherical ball but excluding innermost (and not potential splits) 
-# tuks
+# select Andromeda-like halos, find their positions and compute the distance for each milky way halo to count numbers in spherical ball
 mass_selection_and = (mass_halo > mass_fut_lb) & (mass_halo <= mass_fut_hb)
 andromeda_selection = mass_selection_and & info_this & notsplit_this
 pos_andro = Merger_this['Position'][andromeda_selection]+Lbox/2.
-index_andro = Merger_this['HaloIndex'][andromeda_selection]
 pos_andro %= Lbox
+index_andro = Merger_this['HaloIndex'][andromeda_selection]
 print("Andromeda number = ", pos_andro.shape[0])
 
+# select more massive than Andromeda halos and make sure there are none within 3 Mpc
 mass_selection_hand = (Merger_this['HaloMass'] > mass_fut_hb)
 handromeda_selection = mass_selection_hand & info_this & notsplit_this
 pos_handro = Merger_this['Position'][handromeda_selection]+Lbox/2.
 index_handro = Merger_this['HaloIndex'][handromeda_selection]
 pos_handro %= Lbox
 
-print("Milky Way number = ", position.shape[0])
-print("building tree")
+print("building Andromeda tree")
 tree = spatial.cKDTree(pos_andro, boxsize=Lbox)
-print("building second tree")
+print("building high Andromeda tree")
 treeh = spatial.cKDTree(pos_handro, boxsize=Lbox)
-print("querying tree")
+print("querying trees four times")
 list_hb = np.array(tree.query_ball_point(position, dist_fut_hb))
 list_far = np.array(tree.query_ball_point(position, dist_cl_hb))
 list_lb = np.array(tree.query_ball_point(position, dist_fut_lb))
@@ -305,12 +283,12 @@ assert position.shape[0] == len(list_hb)
 and_inds = np.arange(pos_andro.shape[0], dtype=int)
 mw_inds = np.arange(position.shape[0], dtype=int)
 for i in range(len(list_hb)):
-    if (i % 100000 == 0): print(i)
+    #if (i % 100000 == 0): print(i)
     # how many halos in the andromeda range
     n_and = len(list_hb[i])-len(list_lb[i])
     # how many halos further away
     n_far = len(list_far[i]) - len(list_hb[i])
-    # how many halos close by
+    # how many halos close by (remove self)
     n_lb = len(list_lb[i]) - 1
     # how many halos higher further away
     n_hfar = len(list_hfar[i])
@@ -321,10 +299,6 @@ for i in range(len(list_hb)):
 # disqualify halos that don't have exactly one Andromeda-sized neighbor
 print("percentage MW-sized halos with Andromeda-sized halos beside them = ", np.sum(andromeda)*100./len(andromeda))
 np.save(f"data/andromeda_{sim_name:s}.npy", andromeda)
-# end TESTING
-
-# we check for z_this (and not prev) because they were distinct in the previous redshift
-
 
 # all progenitors
 npout = Merger_this['NumProgenitors'][initial_selection]
@@ -353,7 +327,6 @@ else:
     n_merger, n_merger_above = count_progenitors(npout, nstart, main_progs, progs, masses_prev, offsets_prev, slabs_prev, mass_ms_lb, mass_ms_lb)
     n_merger_above_m1 += n_merger_above
     
-print("looping")
 # loop over redshifts
 for i in range(ind_start+1, ind_stop + 1):
     # current redshift
@@ -371,23 +344,17 @@ for i in range(ind_start+1, ind_stop + 1):
     N_halos_slabs_this, slabs_this = get_halos_per_slab(fns_this, minified=False)
     N_halos_slabs_prev, slabs_prev = get_halos_per_slab(fns_prev, minified=False)
 
-    t1 = time.time()
     # loading tree data
-    mt_data_this = get_mt_info(fns_this, fields=fields_mt_this, minified=False)
+    mt_data_this = get_mt_info(fns_this, fields=fields_mt_this, minified=False) # slowest part
     Merger_this = mt_data_this['merger']
-    print("got data in = ", time.time()-t1)
 
-    print("correcting progs")
-    t1 = time.time()
     # no info is denoted by 0 or -999, but -999 messes with unpacking, so we set it to 0
     Merger_this['MainProgenitor'][Merger_this['MainProgenitor'] < 0] = 0
 
     # rework the main progenitor and halo indices to return in proper order
     main_progs_next = correct_inds(Merger_this['MainProgenitor'][main_progs], N_halos_slabs_prev, slabs_prev, inds_fn_prev)
-    print("corrected in = ", time.time()-t1)
     
     # all progenitors
-    t1 = time.time()
     npout = Merger_this['NumProgenitors'][main_progs]
     nstart = Merger_this['StartProgenitors'][main_progs]
     progs = mt_data_this['progenitors']['Progenitors']
@@ -395,14 +362,10 @@ for i in range(ind_start+1, ind_stop + 1):
     Merger_prev = mt_data_prev['merger']
     masses_prev = Merger_prev['HaloMass']
     masses_prev[Merger_prev['IsPotentialSplit'] == 1] = 0. # set masses of potential splits to zero, , so they don't make cut
-    print("loaded stuff in", time.time()-t1)
 
     # this is for setting up the progenitors stuff
     offsets_prev = np.zeros(len(inds_fn_prev), dtype=np.int64)
     offsets_prev[1:] = np.cumsum(N_halos_slabs_prev)[:-1]
-
-    print("counting progs")
-    t1 = time.time()
 
     # we check for z_this (and not prev) because they were distinct in the previous redshift
     if (z_this in zs_mt[zs_m1]) or (z_this in zs_mt[zs_m2]) or (z_this in zs_mt[zs_m3]):
@@ -421,17 +384,15 @@ for i in range(ind_start+1, ind_stop + 1):
         n_merger, n_merger_above = count_progenitors(npout, nstart, main_progs_next, progs, masses_prev, offsets_prev, slabs_prev, mass_ms_lb, mass_ms_lb)
         # record number of progenitors in mass range of interest (i.e. merger sizes)
         n_merger_above_m1 += n_merger_above
-            
-    print("counted progs in = ", time.time()-t1)
     del mt_data_prev, Merger_prev, masses_prev, progs
 
     # update for next
     main_progs = main_progs_next
     
     # update the main progenitor indices for the next
-    print("halos with no information at z", np.sum(main_progs <= 0), z_this) # there should be no negative ones
     missing[main_progs <= 0] = False
-
+    print("halos with no information at z", np.sum(main_progs <= 0), z_this)
+    
     del mt_data_this, Merger_this
     gc.collect()
 
